75273a16da8e5e33174a39d763c80565a82fac4b
刘一帆 <marvololyf@163.com>
2023-03-21 22:46:40 +0800

    最后修改

--- lib/service/NetworkManager.dart
+++ lib/service/NetworkManager.dart
@@ -1,9 +1,7 @@
 import 'package:dio/dio.dart';
-<<<<<<< Updated upstream
-=======
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ting/model/basic_user_info.dart';
->>>>>>> Stashed changes
+
 
 const String BASEURL = "http://twt.subit.org.cn";
 
@@ -14,13 +12,9 @@ class HTTPConfig {
 
 class LYFHttpRequest {
   static final BaseOptions options = BaseOptions(
-<<<<<<< Updated upstream
     baseUrl: HTTPConfig.baseURL,
     connectTimeout: const Duration(milliseconds: HTTPConfig.timeout),
   );
-=======
-      baseUrl: HTTPConfig.baseURL, connectTimeout: const Duration(microseconds: 3000));
->>>>>>> Stashed changes
   static final Dio dio = Dio(options);
 
   static Future<String?> Requestfriend(String account) async {
@@ -37,16 +31,9 @@ class LYFHttpRequest {
     }
     return response["token"];
   }
-
-<<<<<<< Updated upstream
-  static Future<String?> friendlist() async {
-    var response = (await dio.get(
-      "/friends/getFriendList",
-    ))
-=======
   static Future<String?> getFriendList() async {
     var response = (await dio.get("/friends/getFriendList",))
->>>>>>> Stashed changes
+
         .data;
     if (response["code"] != 10000) {
       return null;
